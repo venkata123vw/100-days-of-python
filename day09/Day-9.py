3670ce572936c1320cffaac5174d33ea4f1e369e
venkata123vw <venkatavarshith100@gmail.com>
2026-06-09 10:08:29 +0530

    Day 9 - Blind Auction Project

---
+++ day09/Day-9.py
@@ -0,0 +1,50 @@
+logo = r"""
+                         ___________
+                         \         /
+                          )_______(
+                          |"""""""|_.-._,.---------.,_.-._
+                          |       | | |               | | ''-.
+                          |       |_| |_             _| |_..-'
+                          |_______| '-' `'---------'` '-'
+                          )"""""""(
+                         /_________\\
+                       .-------------.
+                      /_______________\\
+"""
+
+print(logo)
+
+
+def find_highest_bidder(bidding_dictionary):
+    highest_bid = 0
+    winner = ""
+
+    for bidder in bidding_dictionary:
+        bid_amount = bidding_dictionary[bidder]
+
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+
+    print(f"\nThe winner is {winner} with a bid of ${highest_bid}.")
+
+
+bids = {}
+continue_bidding = True
+
+while continue_bidding:
+    name = input("What is your name? : ")
+    price = int(input("What is your bid? : $"))
+
+    bids[name] = price
+
+    should_continue = input(
+        "Are there any other bidders? Type 'yes' or 'no': "
+    ).lower()
+
+    if should_continue == "no":
+        continue_bidding = False
+        find_highest_bidder(bids)
+
+    elif should_continue == "yes":
+        print("\n" * 100)
