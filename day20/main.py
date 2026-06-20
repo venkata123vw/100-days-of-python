e540dc5cbd9ed5824462a1d1ba05a80656b8b1c8
venkata123vw <venkatavarshith100@gmail.com>
2026-06-20 20:14:21 +0530

    Day 20: Snake Game Part 1 - OOP and Movement

---
+++ day20/main.py
@@ -0,0 +1,27 @@
+from turtle import Screen,Turtle
+from snake import Snake
+import time
+
+screen = Screen()
+screen.setup(600,600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+
+screen.listen()
+screen.onkey(snake.up,"Up")
+screen.onkey(snake.down,"Down")
+screen.onkey(snake.left,"Left")
+screen.onkey(snake.right,"Right")
+
+is_game_on = True
+
+while is_game_on:
+    screen.update()
+    time.sleep(0.1)
+    
+    snake.move()
+    
+screen.exitonclick()
